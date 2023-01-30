4ea010dc2124fe39f1bb38f6314f549ad73ce91e
TreyDrizzle <davistyler.chad@gmail.com>
2023-01-30 16:25:09 -0600

    new dealership

---
+++ Discussion_forum/views.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render,redirect
+from .models import * 
+from .forms import * 
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from datetime import datetime
+import logging as logger
+import json
+# Create your views here.
+
+def home(request):
+    forums=forum.objects.all()
+    count=forums.count()
+    discussions=[]
+    for i in forums:
+        discussions.append(i.discussion_set.all())
+
+    context={'forums':forums,
+              'count':count,
+              'discussions':discussions}
+    return render(request,'home.html',context)
+
+def addInForum(request):
+    form = CreateInForum()
+    if request.method == 'POST':
+        form = CreateInForum(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context ={'form':form}
+    return render(request,'addInForum.html',context)
+
+def addInDiscussion(request):
+    form = CreateInDiscussion()
+    if request.method == 'POST':
+        form = CreateInDiscussion(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context ={'form':form}
+    return render(request,'addInDiscussion.html',context)
+
+def registrationRequest(request):
+    context = {}
+    if request.method == 'GET':
+        return render(request, 'registration.html', context)
+    elif request.method == 'POST':
+        # Check if user exists
+        username = request.POST['username']
+        password = request.POST['psw']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        user_exist = False
+        try:
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            logger.error("New user")
+        if not user_exist:
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
+                                            password=password)
+            user.is_superuser = True
+            user.is_staff=True
+            user.save()  
+            login(request, user)
+            return redirect("index")
+        else:
+            messages.warning(request, "The user already exists.")
+            return redirect("registration")
